9889b439641304dc31b293eacdc94a52ca8b24a3
Nathen Harvey <nharvey@chef.io>
2015-01-13 20:50:32 +0000

    Use a template resource instead of a file

--- chef-repo/cookbooks/motd/recipes/default.rb
+++ chef-repo/cookbooks/motd/recipes/default.rb
@@ -3,9 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-file "/etc/motd" do
+template "/etc/motd" do
   action :create
-  content "Property of Chef Software, Inc.\n"
+  source "motd.erb"
   owner "root"
   mode "0644"
   group "root"
