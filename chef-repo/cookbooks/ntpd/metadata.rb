35256b712f8440ce918fa12e0e265e28acd5f062
Nathen Harvey <nharvey@chef.io>
2015-01-13 21:07:59 +0000

    initial ntpd cookbook

---
+++ chef-repo/cookbooks/ntpd/metadata.rb
@@ -0,0 +1,8 @@
+name             'ntpd'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures ntpd'
+long_description 'Installs/Configures ntpd'
+version          '0.1.0'
+
---
+++ chef-repo/cookbooks/ntpd/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: ntpd
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
