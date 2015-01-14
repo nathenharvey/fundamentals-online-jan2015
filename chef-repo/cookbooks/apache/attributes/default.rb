4c18dceb19dde700662a9de60aa391b28c1418c3
Nathen Harvey <nharvey@chef.io>
2015-01-14 21:47:20 +0000

    Update apache to use a custom greeting

---
+++ chef-repo/cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["greeting"] = "Annapolis"
+default["apache"]["port"] = 81
--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -4,7 +4,13 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package "httpd" do
+apache_package_name = "httpd"
+
+if node["platform"] == "ubuntu"
+  apache_package_name = "apache2"
+end
+
+package apache_package_name do
   action :install
 end
 
